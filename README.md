13779d4bae87ec614bc07bebc9c8f69437720038
Martin Jainta <martin.jainta@tradebyte.com>
2017-11-14 15:00:36 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# PHP coding template
+
+A simple template with PHPUnit set up
+
+# Setup
+
+Just use `make setup` for setup, then use `make test` to start tests.
+
+```
+$ make setup
+$ make test
+```
---
+++ tests/questTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test;
+
+use \PHPUnit\Framework\TestCase;
+use \App\Quest;
+
+class QuestTest extends TestCase {
+    public function testSomething() {
+        $quest = new Quest;
+        $this->assertTrue(true);
+    }
+}
